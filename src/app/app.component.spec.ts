63cf303b3c6e41b07386def3d890da0535225b44
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2024-10-12 17:18:38 +0200

    feat: add prettier

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,7 +15,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should have the \'double-spud\' title', () => {
+  it('should have the double-spud title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('double-spud');
@@ -25,6 +25,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, double-spud');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, double-spud'
+    );
   });
 });
