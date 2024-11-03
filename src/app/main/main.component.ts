1afee6cadbd42cdc7a56881d585a5275da825a27
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2024-11-03 19:55:46 +0100

    feat: add styles to header

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { CardListComponent } from './card-list/card-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CardListComponent],
+  imports: [HeaderComponent, CardListComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
